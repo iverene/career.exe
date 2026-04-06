f0031cd9686ad40cd8fe842a6c0306705aa53c2c
iverene <causapiniverenegrace@gmail.com>
2026-04-06 22:49:21 +0800

    Added the deployed link

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://career-exe.vercel.app"],
     credentials: true
 }));
 
